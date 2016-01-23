684111c170dae079d1eea1ccc3d8edccd1da5d53
Auke van Slooten <auke@muze.nl>
2016-01-23 23:15:21 +0100

    fixed declarations to conform to psr-2

--- src/html.php
+++ src/html.php
@@ -13,21 +13,21 @@ namespace arc;
 
 class html extends xml
 {
-	static private $formBuilder=null;
-	static public $writer=null;	
-		
-	static public function __callStatic( $name, $args ) 
-	{
-		if ( !isset(self::$writer) ) {
-			self::$writer = new html\Writer();
-		}
-		return call_user_func_array( [ self::$writer, $name ], $args );
-	}
+    private static $formBuilder=null;
+    public static $writer=null;    
+        
+    public static function __callStatic( $name, $args ) 
+    {
+        if ( !isset(self::$writer) ) {
+            self::$writer = new html\Writer();
+        }
+        return call_user_func_array( [ self::$writer, $name ], $args );
+    }
 
-	static public function parse( $html=null, $encoding = null ) 
-	{
-		$parser = new html\Parser();
-		return $parser->parse( $html, $encoding );
-	}
+    public static function parse( $html=null, $encoding = null ) 
+    {
+        $parser = new html\Parser();
+        return $parser->parse( $html, $encoding );
+    }
 
 }
